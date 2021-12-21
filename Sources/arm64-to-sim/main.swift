5039e80ec9e44087a398fb97b2bcada2e0bc660e
Patrick Balestra <patrickb@spotify.com>
2021-12-21 10:59:21 +0100

    Fix
    Signed-off-by: Patrick Balestra <patrickb@spotify.com>

--- Sources/arm64-to-sim/main.swift
+++ Sources/arm64-to-sim/main.swift
@@ -60,8 +60,16 @@ enum Transmogrifier {
         
         return (headerData, loadCommandsData, programData)
     }
+
+    private static func applyOffset<T: UnsignedInteger>(_ value: T, _ offset: Int32) -> T {
+        if offset < 0 {
+            let absOffset = T(abs(offset))
+            return absOffset > value ? 0 : value - absOffset
+        }
+        return value + T(offset)
+    }
     
-    private static func updateSegment64(_ data: Data, _ offset: UInt32) -> Data {
+    private static func updateSegment64(_ data: Data, _ offset: Int32) -> Data {
         // decode both the segment_command_64 and the subsequent section_64s
         var segment: segment_command_64 = data.asStruct()
         
@@ -71,14 +79,18 @@ enum Transmogrifier {
         }
         
         // shift segment information by the offset
-        segment.fileoff += UInt64(offset)
-        segment.filesize += UInt64(offset)
-        segment.vmsize += UInt64(offset)
+        segment.fileoff = applyOffset(segment.fileoff, offset)
+        segment.filesize = applyOffset(segment.filesize, offset)
+        segment.vmsize = applyOffset(segment.vmsize, offset)
         
         let offsetSections = sections.map { section -> section_64 in
             var section = section
-            section.offset += UInt32(offset)
-            section.reloff += section.reloff > 0 ? UInt32(offset) : 0
+            section.offset = applyOffset(section.offset, offset)
+            if section.reloff > 0 {
+                section.reloff = applyOffset(section.reloff, offset)
+            } else {
+                section.reloff = 0
+            }
             return section
         }
         
@@ -92,7 +104,7 @@ enum Transmogrifier {
         return datas.merge()
     }
     
-    private static func updateVersionMin(_ data: Data, _ offset: UInt32, minos: UInt32, sdk: UInt32) -> Data {
+    private static func makeBuildVersion(minos: UInt32, sdk: UInt32) -> Data {
         var command = build_version_command(cmd: UInt32(LC_BUILD_VERSION),
                                             cmdsize: UInt32(MemoryLayout<build_version_command>.stride),
                                             platform: UInt32(PLATFORM_IOSSIMULATOR),
@@ -103,16 +115,16 @@ enum Transmogrifier {
         return Data(bytes: &command, count: MemoryLayout<build_version_command>.stride)
     }
     
-    private static func updateDataInCode(_ data: Data, _ offset: UInt32) -> Data {
+    private static func updateDataInCode(_ data: Data, _ offset: Int32) -> Data {
         var command: linkedit_data_command = data.asStruct()
-        command.dataoff += offset
+        command.dataoff = applyOffset(command.dataoff, offset)
         return Data(bytes: &command, count: data.commandSize)
     }
     
-    private static func updateSymTab(_ data: Data, _ offset: UInt32) -> Data {
+    private static func updateSymTab(_ data: Data, _ offset: Int32) -> Data {
         var command: symtab_command = data.asStruct()
-        command.stroff += offset
-        command.symoff += offset
+        command.stroff = applyOffset(command.stroff, offset)
+        command.symoff = applyOffset(command.symoff, offset)
         return Data(bytes: &command, count: data.commandSize)
     }
     
@@ -121,29 +133,49 @@ enum Transmogrifier {
             fatalError("Please add a path to command!")
         }
         let (headerData, loadCommandsData, programData) = readBinary(atPath: path)
-        
-        // `offset` is kind of a magic number here, since we know that's the only meaningful change to binary size
-        // having a dynamic `offset` requires two passes over the load commands and is left as an exercise to the reader
-        let offset = UInt32(abs(MemoryLayout<build_version_command>.stride - MemoryLayout<version_min_command>.stride))
-        
-        let editedCommandsData = loadCommandsData
+
+        var hasVersionMin = false
+        var buildVersionSize: UInt32 = 0
+        for lc in loadCommandsData {
+            let cmd = Int32(lc.loadCommand)
+            if cmd == LC_VERSION_MIN_IPHONEOS {
+                hasVersionMin = true
+            } else if cmd == LC_BUILD_VERSION {
+                buildVersionSize = UInt32(lc.count)
+            }
+        }
+        let hasBuildVersion = (buildVersionSize > 0)
+
+        var offset: Int32 = 0
+        if hasBuildVersion && hasVersionMin {
+            fatalError("has LC_BUILD_VERSION and LC_VERSION_MIN_IPHONEOS")
+        } else if !hasBuildVersion && !hasVersionMin {
+            fatalError("doesn't have LC_BUILD_VERSION or LC_VERSION_MIN_IPHONEOS")
+        } else if hasVersionMin {
+            offset = Int32(MemoryLayout<build_version_command>.stride - MemoryLayout<version_min_command>.stride)
+        } else {
+            offset = Int32(MemoryLayout<build_version_command>.stride) - Int32(buildVersionSize)
+        }
+
+        let editedCommands = loadCommandsData
             .map { (lc) -> Data in
                 switch Int32(lc.loadCommand) {
                 case LC_SEGMENT_64:
-                    return updateSegment64(lc, offset)
+                    return offset == 0 ? lc : updateSegment64(lc, offset)
                 case LC_VERSION_MIN_IPHONEOS:
-                    return updateVersionMin(lc, offset, minos: minos, sdk: sdk)
+                    return makeBuildVersion(minos: minos, sdk: sdk)
                 case LC_DATA_IN_CODE, LC_LINKER_OPTIMIZATION_HINT:
-                    return updateDataInCode(lc, offset)
+                    return offset == 0 ? lc : updateDataInCode(lc, offset)
                 case LC_SYMTAB:
-                    return updateSymTab(lc, offset)
+                    return offset == 0 ? lc : updateSymTab(lc, offset)
                 case LC_BUILD_VERSION:
-                    fatalError("This arm64 binary already contains an LC_BUILD_VERSION load command!")
+                    return makeBuildVersion(minos: minos, sdk: sdk)
                 default:
                     return lc
                 }
             }
-            .merge()
+
+        let editedCommandsData = editedCommands.merge()
         
         var header: mach_header_64 = headerData.asStruct()
         header.sizeofcmds = UInt32(editedCommandsData.count)
